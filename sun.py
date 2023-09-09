145f4d5babb1af3ccf480e8d11af2ba4709ea5cb
Creator754915 <106489587+Creator754915@users.noreply.github.com>
2023-09-09 10:05:00 +0200

    Create sun.py

---
+++ sun.py
@@ -0,0 +1,27 @@
+from ursina import Entity, Vec4
+from panda3d.core import DirectionalLight, AmbientLight
+
+
+class Sun(Entity):
+    def __init__(self, target):
+        super().__init__()
+
+        self.sun = DirectionalLight("sun")
+
+        self.sun.getLens().setNearFar(-120, 300)
+        self.sun.getLens().setFilmSize((150, 150))
+
+        self.pivot = render.attachNewNode(self.sun)
+        self.pivot.lookAt((1,-1,-1))
+        render.setLight(self.pivot)
+
+        self.lighting = AmbientLight('lighting')
+        self.lighting.setColor(Vec4(0.4, 0.5, 0.6, 0) * 1.3)
+        self.parent = render.attachNewNode(self.lighting)
+        render.setLight(self.parent)
+
+        self.target = target
+
+    def update(self):
+        self.sun.setShadowCaster(True, 4096, 4096)
+        self.pivot.setPos(self.target.world_position)
