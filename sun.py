801b3da4a0d19f72377a4334ce36c660f715a02c
Creator754915 <106489587+Creator754915@users.noreply.github.com>
2023-11-12 19:44:05 +0100

    Update sun.py

--- sun.py
+++ sun.py
@@ -1,18 +1,18 @@
-from ursina import Entity, Vec4
-from panda3d.core import DirectionalLight, AmbientLight
+from direct.task.TaskManagerGlobal import taskMgr
+from panda3d.core import Vec4, DirectionalLight, AmbientLight
 
 
-class Sun(Entity):
+class Sun:
     def __init__(self, target):
         super().__init__()
 
-        self.sun = DirectionalLight("sun")
+        self.sun = DirectionalLight('sun')
 
         self.sun.getLens().setNearFar(-120, 300)
         self.sun.getLens().setFilmSize((150, 150))
 
         self.pivot = render.attachNewNode(self.sun)
-        self.pivot.lookAt((1,-1,-1))
+        self.pivot.lookAt((1, -1, -1))
         render.setLight(self.pivot)
 
         self.lighting = AmbientLight('lighting')
@@ -22,6 +22,10 @@ class Sun(Entity):
 
         self.target = target
 
-    def update(self):
+        taskMgr.add(self.update, 'update')
+
+    def update(self, task):
         self.sun.setShadowCaster(True, 4096, 4096)
         self.pivot.setPos(self.target.world_position)
+
+        return task.cont
