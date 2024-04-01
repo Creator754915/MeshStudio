7347bcde4ad7d3f93a4087ef0dc8f9dd42180bdb
Creator754915 <106489587+Creator754915@users.noreply.github.com>
2024-04-01 22:52:19 +0200

    Update SoundEditor.py

--- SoundEditor/SoundEditor.py
+++ SoundEditor/SoundEditor.py
@@ -2,13 +2,32 @@ from ursina import *
 
 
 class SoundEditor(Entity):
-    def __init__(self):
-        super(SoundEditor, self).__init__()
+    def __init__(self, color=color.black10, z=999, **kwargs):
+        super().__init__(model=Quad(radius=.0), color=color, z=z, **kwargs)
+        self.parent = camera.ui
+        self.wdw = window.size.x
+        self.wdh = window.size.y
+        self.qoef = 100
+        self.scale = (self.wdw, 25/self.qoef)
+        self.rotation = self.rotation = (0, 90, 0)
+
+        if 'color' in kwargs:
+            setattr(self, 'color', kwargs['color'])
+
+        self.timeline = Entity(parent=camera.ui, model='plane', rotation=(0, 0, 90), y=window.bottom,
+                               scale=(1, 0.25),
+                               color=Color(0, 0, 0, 1))
 
 
 if __name__ == "__main__":
     app = Ursina()
+    window.borderless = False
+    window.cog_menu.enabled = False
+    window.exit_button.enabled = False
+    window.fps_counter.enabled = False
 
-    SoundEditor()
+    test = SoundEditor()
+
+    EditorCamera()
 
     app.run()
